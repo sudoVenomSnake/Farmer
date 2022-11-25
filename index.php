662fc509842492710157f9e1b24475467912144f
sudoVenomSnake <swastikm26@gmail.com>
2022-11-25 17:17:21 +0530

    Third Commit

--- index.php
+++ index.php
@@ -1,167 +1,52 @@
+<?php
+
+session_start();
+
+if(isset($_SESSION["user_id"]))
+{
+    $mysqli = require __DIR__ . "/database.php";
+
+    $sql = "SELECT * FROM user
+            WHERE id = {$_SESSION["user_id"]}";
+
+    $result = $mysqli->query($sql);
+
+    $user = $result->fetch_assoc();
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <title>FMS \ (•◡•) /</title>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Farmer Management System</title>
-    <!-- Font Awesome -->
-    <link
-    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
-    rel="stylesheet"
-    />
-    <!-- Google Fonts -->
-    <link
-    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-    rel="stylesheet"
-    />
-    <!-- MDB -->
-    <link
-    href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.0.1/mdb.min.css"
-    rel="stylesheet"
-    />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/holiday.css@0.9.8" />
 </head>
 <body>
 
-    <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <!-- Container wrapper -->
-    <div class="container-fluid">
-        <!-- Navbar brand -->
-        <!-- <a class="navbar-brand" href="#">Navbar</a> -->
+    <h1>FMS \ (•◡•) /</h1>
 
-        <!-- Toggle button -->
-        <button class="navbar-toggler" type="button" data-mdb-toggle="collapse"
-        data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-        aria-label="Toggle navigation">
-        <i class="fas fa-bars text-light"></i>
-        </button>
+    <?php if(isset($user)): ?>
 
-        <!-- Collapsible wrapper -->
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <!-- Left links -->
-        <ul class="navbar-nav me-auto d-flex flex-row mt-3 mt-lg-0">
-            <li class="nav-item text-center mx-2 mx-lg-1">
-            <a class="nav-link active" aria-current="page" href="#!">
-                <div>
-                <i class="fas fa-home fa-lg mb-1"></i>
-                </div>
-                Home
-            </a>
-            </li>
-            <li class="nav-item text-center mx-2 mx-lg-1">
-            <a class="nav-link" href="#!">
-                <div>
-                <i class="far fa-envelope fa-lg mb-1"></i>
-                <span class="badge rounded-pill badge-notification bg-danger">11</span>
-                </div>
-                Link
-            </a>
-            </li>
-            <li class="nav-item text-center mx-2 mx-lg-1">
-            <a class="nav-link disabled" aria-disabled="true" href="#!">
-                <div>
-                <i class="far fa-envelope fa-lg mb-1"></i>
-                <span class="badge rounded-pill badge-notification bg-warning">11</span>
-                </div>
-                Disabled
-            </a>
-            </li>
-            <li class="nav-item dropdown text-center mx-2 mx-lg-1">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-mdb-toggle="dropdown"
-                aria-expanded="false">
-                <div>
-                <i class="far fa-envelope fa-lg mb-1"></i>
-                <span class="badge rounded-pill badge-notification bg-primary">11</span>
-                </div>
-                Dropdown
-            </a>
-            <!-- Dropdown menu -->
-            <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li>
-                <hr class="dropdown-divider" />
-                </li>
-                <li>
-                <a class="dropdown-item" href="#">Something else here</a>
-                </li>
-            </ul>
-            </li>
-        </ul>
-        <!-- Left links -->
+        <p>Name - <?= htmlspecialchars($user["name"]) ?></p>
+        <p>Email - <?= htmlspecialchars($user["email"]) ?></p>
+        <p>Aadhar - <?= htmlspecialchars($user["aadhar"]) ?></p>
+        <p>City/Village/Taluka - <?= htmlspecialchars($user["city_village_taluka"]) ?></p>
+        <p>State - <?= htmlspecialchars($user["state"]) ?></p>
+        <p>Date of Birth - <?= htmlspecialchars($user["dob"]) ?></p>
+        <p>Pincode - <?= htmlspecialchars($user["pincode"]) ?></p>
+        <p>Blood Group - <?= htmlspecialchars($user["blood_group"]) ?></p>
+        <p>Phone Number - <?= htmlspecialchars($user["phone_number"]) ?></p>
 
-        <!-- Right links -->
-        <ul class="navbar-nav ms-auto d-flex flex-row mt-3 mt-lg-0">
-            <li class="nav-item text-center mx-2 mx-lg-1">
-            <a class="nav-link" href="#!">
-                <div>
-                <i class="fas fa-bell fa-lg mb-1"></i>
-                <span class="badge rounded-pill badge-notification bg-info">11</span>
-                </div>
-                Messages
-            </a>
-            </li>
-            <li class="nav-item text-center mx-2 mx-lg-1">
-            <a class="nav-link" href="#!">
-                <div>
-                <i class="fas fa-globe-americas fa-lg mb-1"></i>
-                <span class="badge rounded-pill badge-notification bg-success">11</span>
-                </div>
-                News
-            </a>
-            </li>
-        </ul>
-        <!-- Right links -->
+        <p><a href = "logout.php">Log out.</a></p>
 
-        <!-- Search form -->
-        <form class="d-flex input-group w-auto ms-lg-3 my-3 my-lg-0">
-            <input type="search" class="form-control" placeholder="Search" aria-label="Search" />
-            <button class="btn btn-primary" type="button" data-mdb-ripple-color="dark">
-            Search
-            </button>
-        </form>
-        </div>
-        <!-- Collapsible wrapper -->
-    </div>
-    <!-- Container wrapper -->
-    </nav>
-    <!-- Navbar -->
+    <?php else: ?>
+        <p><a href = "login.php">Log in</a> or <a href = "signup.html">Sign up.</a></p>
 
-    <div>
-        <div class = "container-fluid">
-            <div class = "row">
-                <div class = "col-lg-6">
-                    <h1 class = "display-3">Farmer Management System</h1>
-                    <p>Hey hi guys welcome to the dashboard</p>
-                    <a href = "" class = "btn btn-lg btn-primary">Button</a>
-                </div>
-                <div class = "w-50 mx-auto card">
-                    <div class = "card-body">
-                        <form action = "" method = "post" class = "">
-                            <div class = "md-form">
-                                <input type="text" id="typeText" class="form-control" />
-                                <label class="form-label" for="typeText">Text input</label>
-                            </div>
-                            <div class = "md-form">
-                                <input type="email" id="email" class="form-control" />
-                                <label class="form-label" for="email">Enter Email</label>
-                            </div>
-                            <div class = "md-form">
-                                <input type="text" id="mobile" class="form-control" />
-                                <label class="form-label" for="mobile">Enter Mobile Number</label>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <?php endif; ?>
 
-    <!-- MDB -->
-    <script
-    type="text/javascript"
-    src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.0.1/mdb.min.js"
-    ></script>
 </body>
 </html>
--- login.php
+++ login.php
@@ -18,6 +18,12 @@ if($_SERVER["REQUEST_METHOD"] === "POST")
     {
         if(password_verify($_POST["password"], $user["password_hash"]))
         {
+            session_start();
+
+            session_regenerate_id();
+            
+            $_SESSION["user_id"] = $user["id"];
+
             header("Location: index.php");
             exit;
         }
@@ -31,29 +37,39 @@ if($_SERVER["REQUEST_METHOD"] === "POST")
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Login</title>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/holiday.css@0.9.8" />
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login</title>
+  <link rel="stylesheet" href="logon.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
 </head>
 <body>
-
-    <h1>Login</h1>
-
-    <?php if($is_invalid): ?>
-        <em>Invalid Login</em>
-    <?php endif; ?>
-
+  <div class="wrapper">
+    <header>Login Form</header>
     <form method = "post">
-        <label for = "email">Email</label>
-        <input type = "email" name = "email" id = "email"
-            value = "<?= htmlspecialchars($_POST["email"] ?? "") ?>">
-
-        <label for = "password">Password</label>
-        <input type = "password" name = "password" id = "password">
-
-        <button>Log In</button>
+      <div class="field email">
+        <div class="input-area">
+          <input type="text" placeholder="Email Address" name = "email" id = "email">
+          <i class="icon fas fa-envelope"></i>
+          <i class="error error-icon fas fa-exclamation-circle"></i>
+        </div>
+        <div class="error error-txt">Email can't be blank</div>
+      </div>
+      <div class="field password">
+        <div class="input-area">
+          <input type="password" placeholder="Password" name = "password" id = "Password">
+          <i class="icon fas fa-lock"></i>
+          <i class="error error-icon fas fa-exclamation-circle"></i>
+        </div>
+        <div class="error error-txt">Password can't be blank</div>
+      </div>
+      <div class="pass-txt"><a href="#">Forgot password?</a></div>
+      <input type="submit" value="Login">
     </form>
+    <div class="sign-txt">Not yet member? <a href="#">Signup now</a></div>
+  </div>
+
+  <script src="script.js"></script>
+
 </body>
 </html>
